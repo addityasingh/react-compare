d67c023ea5448c27d74fd12e1125e15f1189a0be
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-10-05 11:11:48 +0200

    Add fastify server for SSR

--- react16/server/fastify/index.js
+++ react16/server/fastify/index.js
@@ -1,16 +1,21 @@
 const fastify = require('fastify')()
+const { renderToString } = require('react-dom/server');
+import React from 'react';
+import App from './components/App.js';
 
-const server = () => {
-    // Declare a route
-  fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-  })
-  
-  // Run the server!
-  fastify.listen(3000, function (err) {
-    if (err) throw err
-    console.log(`server listening on ${fastify.server.address().port}`)
-  })
-};
+// Declare a route
+fastify.get('/', function (request, response) {
+    const resp = `
+    <!DOCTYPE html><html><head><title>My Page</title></head><body>
+    <div id="root">
+    ${renderToString(<App/>)}
+    </div></body></html>
+    `;
+  response.send(resp);
+})
 
-module.exports = server;
+// Run the server!
+fastify.listen(3000, function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+});
